281262ad7e43f06dbe1aec8890a7ad4c116f6a81
Ber30 <makoberobertrand@gmail.com>
2022-09-11 07:39:03 +0200

    gushiramwo amavariables muri template (Django template language)

--- django-web-app/merchex/listings/views.py
+++ django-web-app/merchex/listings/views.py
@@ -7,7 +7,7 @@ def hello(request):
     bands = Band.objects.all()
     listing = Listing.objects.all()
     #to get the objects
-    return render(request, 'listings/hello.html')
+    return render(request,'listings/hello.html',{'bands': bands})
 
 def about(request):
     return HttpResponse('<h1>About Us</h1> <p>We love merch!</p>')
